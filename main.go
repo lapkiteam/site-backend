ffe319d6f65a42a47a8c604d048afb731133cc48
Bogdan L'gotin <dreamwayx64@gmail.com>
2025-02-08 23:49:21 +0600

    Released Post routes for upload data for games and docs.

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/lapkiteam/site-backend/app/auth"
 	"github.com/lapkiteam/site-backend/app/user"
 	"github.com/lapkiteam/site-backend/pkg/database"
 	"gorm.io/gorm"
+	"log"
 	"net/http"
+	"os"
+	"os/exec"
 )
 
 func Migrate(db *gorm.DB) {
@@ -38,7 +42,85 @@ func main() {
 	authorized := router.Group("/", auth.Auth())
 	{
 		authorized.StaticFS("/MissingBoar.Docs/", gin.Dir("site/docs/MissingBoar", false))
+		authorized.StaticFS("/MissingBoar/", gin.Dir("site/games/MissingBoar", false))
 	}
+
+	router.POST("/MissingBoar.Docs/", func(ctx *gin.Context) {
+		//Recieve File
+		file, _ := ctx.FormFile("file")
+
+		ctx.SaveUploadedFile(file, file.Filename)
+
+		//Remove old docs
+		files, err := os.ReadDir("site/docs/MissingBoar/")
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, file := range files {
+			if file.IsDir() {
+				os.RemoveAll("site/docs/MissingBoar/" + file.Name())
+			}
+
+			if file.Name() != ".gitignore" && file.IsDir() != true {
+				os.Remove("site/docs/MissingBoar/" + file.Name())
+			}
+		}
+
+		//Unpack Tar
+		pwd, _ := os.Getwd()
+
+		cmd := exec.Command("tar", "xf", pwd+"/"+file.Filename, "-C", pwd+"/site/docs/MissingBoar/")
+		stdoutStderr, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", stdoutStderr)
+
+		//Delete tar after unpack
+		e := os.Remove(file.Filename)
+		if e != nil {
+			log.Fatal(e)
+		}
+
+		//Return response
+		ctx.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+	})
+
+	router.POST("/MissingBoar/", func(ctx *gin.Context) {
+		file, _ := ctx.FormFile("file")
+
+		ctx.SaveUploadedFile(file, file.Filename)
+
+		files, err := os.ReadDir("site/games/MissingBoar/")
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, file := range files {
+			if file.IsDir() {
+				os.RemoveAll("site/games/MissingBoar/" + file.Name())
+			}
+
+			if file.Name() != ".gitignore" && file.IsDir() != true {
+				os.Remove("site/games/MissingBoar/" + file.Name())
+			}
+		}
+
+		pwd, _ := os.Getwd()
+
+		cmd := exec.Command("tar", "xf", pwd+"/"+file.Filename, "-C", pwd+"/site/games/MissingBoar/")
+		stdoutStderr, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", stdoutStderr)
+
+		e := os.Remove(file.Filename)
+		if e != nil {
+			log.Fatal(e)
+		}
+
+		ctx.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+	})
 
 	router.Run(":8080")
 }
